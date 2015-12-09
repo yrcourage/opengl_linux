b64e61780cb0624a07d5f506f6a5dcadee65666e
yrcourage <756595055@qq.com>
2015-12-09 21:25:26 +0800

    实现了简单的纹理

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
+#define BUFFER_OFFSET(offset) ((void *)(offset))
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
-#include <stdio.h>
+//#include <stdio.h>
+#include <tiffio.h>
+#include <iostream>
+
+#include "shader.h"
+
+using namespace std;
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+GLFWwindow* window;
+GLuint base_prog;
+
+GLuint VBO, VAO, EBO;
+
+float aspect;
+
+GLuint vao;
+
+GLuint quad_vbo;
+
+GLuint tex;
+
+Shader *s;
+
 static void error_callback(int error, const char* description)
 {
     fputs(description, stderr);
@@ -11,47 +38,159 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
+void read_image_to_block(string path, uint32 **raster, GLuint& width, GLuint& height) {
+
+//    GLuint num = 1;
+//    GLuint Buffers[1];
+//
+//    glGenBuffers(num, Buffers);
+    TIFFSetWarningHandler(0);
+    TIFF *tif = TIFFOpen(path.c_str(), "r");
+
+    if (tif) {
+        size_t npixels;
+
+        TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);
+        TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);
+        npixels = width * height;
+        *raster = (uint32 *) _TIFFmalloc(npixels * sizeof(uint32));
+        if (*raster != NULL) {
+            if (TIFFReadRGBAImageOriented(tif, width, height, *raster, ORIENTATION_TOPLEFT)) {
+
+//                cout<< _msize(*raster)<<endl;
+            }
+//            _TIFFfree(raster);
+        }
+
+        TIFFClose(tif);
+    }
+}
+
+void init()
+{
+    uint32 *raster = NULL;
+    GLuint width=0, height;//height的值是不确定的
+    read_image_to_block("/d/data/test512/512_slice20317.tif", &raster, width, height);
+
+    base_prog = glCreateProgram();
+
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexStorage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Set texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, raster);
+
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+
+    s =new Shader("/d/project/C++/opengl_linux/texture.vs", "/d/project/C++/opengl_linux/texture.frag");
+
+    // Set up vertex data (and buffer(s)) and attribute pointers
+    GLfloat vertices[] = {
+            // Positions          // Colors           // Texture Coords
+            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
+            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
+    };
+    GLuint indices[] = {  // Note that we start from 0!
+            0, 1, 3, // First Triangle
+            1, 2, 3  // Second Triangle
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+    // TexCoord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0); // Unbind VAO
+}
+
+void display()
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    // Bind Textures using texture units
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glUniform1i(glGetUniformLocation((*s).Program, "ourTexture1"), 0);
+    glActiveTexture(GL_TEXTURE1);
+
+    // Activate shader
+    (*s).Use();
+
+    // Draw container
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
+
+
 int main(void)
 {
-    GLFWwindow* window;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Simple example", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
-	glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
     glfwSetKeyCallback(window, key_callback);
+
+    glewExperimental = GL_TRUE;
+
+    //glew一定要在调用任何opengl方法之前初始化，否则会报segment fault
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    init();
     while (!glfwWindowShouldClose(window))
     {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
         glfwSwapBuffers(window);
+        display();
         glfwPollEvents();
     }
-	glfwDestroyWindow(window);
+    glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
 }
